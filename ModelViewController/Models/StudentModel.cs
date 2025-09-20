e3758080ad70fc2d2f7453ff3a852e97d340c499
Prerak Shah <prerak9359@gmail.com>
2025-09-21 00:17:15 +0930

    Working with Age and Range validators

--- ModelViewController/Models/StudentModel.cs
+++ ModelViewController/Models/StudentModel.cs
@@ -11,8 +11,16 @@ namespace ModelViewController.Models
     {
         //public int rollNo { get; set; }
         [Required(ErrorMessage ="Please Enter Correct Name")]
+
         [StringLength(15, MinimumLength = 3)]
         public string Name { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Age is Must")]
+        [Range(10, 50, ErrorMessage = "Age must between 10 and 50")]
+        public int? Age { get; set; }
 
         //public string Gender { get; set; }
 
